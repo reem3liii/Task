56b12078df785862d2ba0a4d75e4f8c3a09a5498
Reem Ali <reemalielesh@gmail.com>
2020-12-19 15:20:55 +0200

    use DOM tree to handle user choice (if statement)

--- taskJS.js
+++ taskJS.js
@@ -1,7 +1,11 @@
 var choice = prompt('Enter 1 for addition and 2 for multiplication')
 
 if (choice == 1) {
+    document.getElementById('title').textContent='Addition of 3';
+    document.getElementById('table').innerHTML='<p>1+3=4<br/>2+3=5<br/>3+3=6<br/>4+3=7<br/>5+3=8<br/>6+3=9<br/>7+3=10<br/>8+3=11<br/>9+3=12<br/>10+3=13<p/>';
 
 }else {
+    document.getElementById('title').textContent='Multiplication of 3';
+    document.getElementById('table').innerHTML='<p>1*3=3<br/>2*3=6<br/>3*3=9<br/>4*3=12<br/>5*3=15<br/>6*3=18<br/>7*3=21<br/>8*3=24<br/>9*3=27<br/>10*3=30<p/>';
 
 }
