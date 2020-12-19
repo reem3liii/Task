1f5aaffe332bc391f85e60153a6ef4b21ce66b6a
Reem Ali <reemalielesh@gmail.com>
2020-12-19 16:24:33 +0200

    adding the title css properties & values

--- taskJS.js
+++ taskJS.js
@@ -6,6 +6,6 @@ if (choice == 1) {
 
 }else {
     document.getElementById('title').textContent='Multiplication of 3';
-    document.getElementById('table').innerHTML='<p>1*3=3<br/>2*3=6<br/>3*3=9<br/>4*3=12<br/>5*3=15<br/>6*3=18<br/>7*3=21<br/>8*3=24<br/>9*3=27<br/>10*3=30<p/>';
+    document.getElementById('table').innerHTML='<p>1&#215;3=3<br/>2&#215;3=6<br/>3&#215;3=9<br/>4&#215;3=12<br/>5&#215;3=15<br/>6&#215;3=18<br/>7&#215;3=21<br/>8&#215;3=24<br/>9&#215;3=27<br/>10&#215;3=30<p/>';
 
 }
